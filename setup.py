82af0367b0b25bb54770b5f1a7f19c244e01ce00
amansrivastava17 <amans.rlx@gmail.com>
2019-07-22 17:39:58 +0530

    added include package data

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/zihangdai/xlnet",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache-2.0 License",
