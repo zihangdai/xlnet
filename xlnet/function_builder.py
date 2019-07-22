db5f02ba08ae6f7f2ea268f531fcda98f1eb8d90
amansrivastava17 <amans.rlx@gmail.com>
2019-07-22 18:17:30 +0530

    update arch

--- xlnet/function_builder.py
+++ xlnet/function_builder.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import functools
 import os
 import tensorflow as tf
 import modeling
