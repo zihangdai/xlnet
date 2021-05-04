bbc6b392065866e19c28d5617e63f1330dd74c65
Tomasz Wiaderek <t.wiaderwek@gmail.com>
2021-05-04 12:26:07 +0200

    Add missing class-score value

--- run_classifier.py
+++ run_classifier.py
@@ -399,8 +399,12 @@ class IStsProcessor(GLUEProcessor):
         self.text_b_column = 2
 
     def get_labels(self):
-        return ["EQUI-5", "OPPO-1", "OPPO-2", "OPPO-2", "OPPO-3", "OPPO-4", "SPE1-1", "SPE1-2", "SPE1-3", "SPE1-4", "SPE2-1", "SPE2-2", "SPE2-3", "SPE2-4",
-                "SIMI-1", "SIMI-2", "SIMI-3", "SIMI-4", "REL -1", "REL -2", "REL -3", "REL -4", "NOALI-0", "ALIC-0"]
+        return ["EQUI-5", "OPPO-1", "OPPO-2", "OPPO-2", "OPPO-3", "OPPO-4",
+                "SPE1-1", "SPE1-2", "SPE1-3", "SPE1-4", "SPE1-5",
+                "SPE2-1", "SPE2-2", "SPE2-3", "SPE2-4", "SPE2-5",
+                "SIMI-1", "SIMI-2", "SIMI-3", "SIMI-4",
+                "REL -1", "REL -2", "REL -3", "REL -4",
+                "NOALI-0", "ALIC-0"]
 
     def _create_examples(self, lines, set_type):
         examples = []
@@ -690,7 +694,7 @@ def main(_):
       'sts-b': StsbProcessor,
       'imdb': ImdbProcessor,
       "yelp5": Yelp5Processor,
-      "ists" : IStsProcessor
+      "ists": IStsProcessor
   }
 
   if not FLAGS.do_train and not FLAGS.do_eval and not FLAGS.do_predict:
